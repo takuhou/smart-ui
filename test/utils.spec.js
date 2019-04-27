5ff360df7ee33eaeabe943876af18cc252f6f929
takuho.sanpei <takuho.sanpei@gmail.com>
2019-04-28 03:43:43 +0900

    utilsのtest追加

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -1,23 +1,71 @@
 const assert = require("assert");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const Utils = require("../src/utils.js");
-
-const resourceLoader = new jsdom.ResourceLoader({
-  userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
-});
-const { window } = new JSDOM(``, { resources: resourceLoader });
+import Utils from "../src/utils.js";
 
 describe('Utils', () => {
   it('useragentの取得', () => {
-    const ua = Utils.ua;
-    assert.equal('mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/73.0.3683.103 safari/537.36', ua);
+    const resourceLoader = new jsdom.ResourceLoader({
+      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
+    });
+    global.window = new JSDOM(``, { resources: resourceLoader }).window;
+    assert.equal(window.navigator.userAgent.toLowerCase(), Utils.ua);
   });
 
-  it('chrome判定', () => {
-    const ua = window.navigator.userAgent.toLocaleLowerCase();
-    assert.equal('mozilla/5.0 (windows nt 10.0; win64; x64) applewebkit/537.36 (khtml, like gecko) chrome/73.0.3683.103 safari/537.36', ua);
+  it('Chrome判定', () => {
+    const resourceLoader = new jsdom.ResourceLoader({
+      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
+    });
+    global.window = new JSDOM(``, { resources: resourceLoader }).window;
+    assert.equal(true, Utils.isChrome());
   });
 
+  it('Safari判定', () => {
+    const resourceLoader = new jsdom.ResourceLoader({
+      userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15"
+    });
+    global.window = new JSDOM(``, { resources: resourceLoader }).window;
+    assert.equal(true, Utils.isSafari());
+  });
+
+  it('Firefox判定', () => {
+    const resourceLoader = new jsdom.ResourceLoader({
+      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
+    });
+    global.window = new JSDOM(``, { resources: resourceLoader }).window;
+    assert.equal(true, Utils.isFirefox());
+  });
+
+  it('IE判定', () => {
+    const resourceLoader = new jsdom.ResourceLoader({
+      userAgent: "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729; rv:11.0) like Gecko"
+    });
+    global.window = new JSDOM(``, { resources: resourceLoader }).window;
+    assert.equal(true, Utils.isIe());
+  });
+
+  it('Edge判定', () => {
+    const resourceLoader = new jsdom.ResourceLoader({
+      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; ServiceUI 13.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134"
+    });
+    global.window = new JSDOM(``, { resources: resourceLoader }).window;
+    assert.equal(true, Utils.isEdge());
+  });
+
+  it('hasConicGradient対応ブラウザ', () => {
+    const resourceLoader = new jsdom.ResourceLoader({
+      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
+    });
+    global.window = new JSDOM(``, { resources: resourceLoader }).window;
+    assert.equal(true, Utils.hasConicGradient());
+  });
+
+  it('hasConicGradient非対応ブラウザ', () => {
+    const resourceLoader = new jsdom.ResourceLoader({
+      userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; ServiceUI 13.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134"
+    });
+    global.window = new JSDOM(``, { resources: resourceLoader }).window;
+    assert.equal(false, Utils.hasConicGradient());
+  });
 
 });
